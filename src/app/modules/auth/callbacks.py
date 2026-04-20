f2c4981a6fea40270fe19143ef43314827253f0b
paulwinex <paulwinex@gmail.com>
2026-04-20 11:55:49 +0300

    fix example callback

--- src/app/modules/auth/callbacks.py
+++ src/app/modules/auth/callbacks.py
@@ -1,9 +1,9 @@
 from app.core.events import get_event_router
-from app.modules.users.schemas import UserSchema
+from app.modules.users.schemas import UserEvent
 
 roter = get_event_router()
 
 @roter.subscriber('auth.user-logged-in')
-async def on_user_logged_in(user: UserSchema):
+async def on_user_logged_in(user: UserEvent):
     """Callbacks example"""
     print('USER LOGGED IN', user)
